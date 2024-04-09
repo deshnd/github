fd236e712203fccf879d54374505125589c2c0be
eshaan <92547104+deshnd@users.noreply.github.com>
2024-04-08 22:04:38 -0500

    Update here.swift

--- here.swift
+++ here.swift
@@ -1,6 +1,18 @@
 
 // Add your code below
-
+import Foundation
+func readLines() -> [String] {
+    var lines = [String]()
+    while let line = readLine() {
+        lines += [line]
+    }
+    return lines
+}
+func printLines(lines: [String]) {
+    for line in lines {
+        print(line)
+    }
+}
 // Add your code above
 
 // ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
